2b5ea2104d259301c889b8513fcb02176d96966b
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-03-26 17:11:39 +0800

    fix: remove extraneous preload.py

--- preload.py
+++
@@ -1,7 +0,0 @@
-def preload(parser):
-    parser.add_argument(
-        "--metadata-prefix", type=str, help="Prefix to metadata text", default=None
-    )
-    parser.add_argument(
-        "--metadata-suffix", type=str, help="Suffix to metadata text", default=None
-    )
